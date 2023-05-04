d3fc2c1064509379de9d6cbb6e46b3b5ab963b0e
iamthisknowledge <rodrigoteixeira@Rodrigos-iPro.lan>
2023-05-04 15:52:20 +0200

    about Datatypes & Typecasting in python

--- datatypes-and-typecasting/index.py
+++ datatypes-and-typecasting/index.py
@@ -1,9 +1,13 @@
-print('Variables & Datatypes - Exercise')
-#Create appropriate Variables for Item name, the price 
-#and how many you have in stock
-item_name = 'Mdutti'
-price = 2.90
-stock = 5
-is_in_inventory_madeira = True
+a = int(1)        # a will be 1
+b = int(2.5)      # b will be 2
+c = int("3")      # c will be 3
+c1 = int(float("3.4"))   # c1 will be...
+d = float(1)      # d will be 1.0
+e = float(2.5)    # e will be 2.5
+f = float("3")    # f will be 3.0
+g = float("4.23") # g will be 4.23
+h = str("80s")    # h will be '80s'
+i = str(22)       # i will be '22'
+j = str(3.01)     # j will be '3.01'
 
-print([item_name,price,stock])
+print([a,b,c,c1,d,e,f,g,h,i,j])
